82e3b29c9b41998f18410e293b582285d66c5999
Shomen Banerjee <shomenbanerjee@hotmail.com>
2025-07-19 23:54:14 +0100

    Delete utility.py

--- utility.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_draws(file_path_or_buffer):
-    df = pd.read_csv(file_path_or_buffer)
-    draws = df[[col for col in df.columns if col.lower().startswith("draw")]].values.tolist()
-    return draws, df
